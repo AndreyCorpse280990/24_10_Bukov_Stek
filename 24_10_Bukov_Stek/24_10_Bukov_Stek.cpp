82494b41b53e0bc2049f3f91076131f9ed427a13
AndreyCorpse28090 <280990corpse@gmail.com>
2023-11-04 15:13:44 +0300

    Добавьте файлы проекта.

---
+++ 24_10_Bukov_Stek/24_10_Bukov_Stek.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+// stack-based function for string validation
+void stack(const std::string &input)
+{
+	std::string text = input;
+	if (!text.empty())
+	{
+		if (text.find("{") && text.find("[") && text.find("("))
+		{
+
+		}
+	}
+	else
+	{
+		std::cout << "Строка пустая" << std::endl;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	std::string input;
+	
+}
